2d905e0f809fb81431207a8426fb056e314bb0f6
Eric Santos <eric.santos@westwing.com.br>
2020-05-17 16:38:04 -0300

    Start Models

---
+++ database/factories/CampaingFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Campaing;
+use Faker\Generator as Faker;
+
+$factory->define(Campaing::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/seeds/CampaingSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CampaingSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
